f2b1e646c2405c8d02914b11c1431869c4d05e2b
Jad Wehbeh <jadwehbeh97@gmail.com>
2021-11-10 14:39:23 -0500

    Add test for consistency check

--- test/test.cpp
+++ test/test.cpp
@@ -28,10 +28,7 @@ TEST_CASE("Hover")
   test_controller.solve();
   std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
   std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
-  REQUIRE((std::abs(test_controller.u(0) - 2.4525) < 0.05 &&
-      std::abs(test_controller.u(1) - 2.4525) < 0.05 &&
-      std::abs(test_controller.u(2) - 2.4525) < 0.05 &&
-      std::abs(test_controller.u(3) - 2.4525) < 0.05));
+  REQUIRE((std::abs(test_controller.u(0) - 2.4525) < 0.05 && std::abs(test_controller.u(1) - 2.4525) < 0.05 && std::abs(test_controller.u(2) - 2.4525) < 0.05 && std::abs(test_controller.u(3) - 2.4525) < 0.05));
 };
 
 TEST_CASE("Inverted Hover")
@@ -57,18 +54,16 @@ TEST_CASE("Inverted Hover")
   test_controller.solve();
   std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
   std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
-  REQUIRE((std::abs(test_controller.u(0) + 2.4525) < 0.05 &&
-      std::abs(test_controller.u(1) + 2.4525) < 0.05 &&
-      std::abs(test_controller.u(2) + 2.4525) < 0.05 &&
-      std::abs(test_controller.u(3) + 2.4525) < 0.05));
+  REQUIRE((std::abs(test_controller.u(0) + 2.4525) < 0.05 && std::abs(test_controller.u(1) + 2.4525) < 0.05 && std::abs(test_controller.u(2) + 2.4525) < 0.05 && std::abs(test_controller.u(3) + 2.4525) < 0.05));
 };
 
-TEST_CASE("Runtime")
+TEST_CASE("Consistency")
 {
   Eigen::VectorXd x_lin = Eigen::VectorXd(18);
   Eigen::VectorXd u_lin = Eigen::VectorXd(4);
+  Eigen::VectorXd u_init = Eigen::VectorXd(4);
   Eigen::VectorXd Y = Eigen::VectorXd::Zero(8 * 12);
-  x_lin << 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0.;
+  x_lin << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.7071, -0.7071, 0., 0.6830, 0.6830, -0.2588, 0.1830, 0.1830, 0.9659, 0.1, 0.1, 0.1;
   u_lin << 2.4525, 2.4525, 2.4525, 2.4525;
   for (int i = 0; i < 8; i++) {
     Y(i * 12 + 3) = 1.;
@@ -84,9 +79,14 @@ TEST_CASE("Runtime")
   test_controller.discretize();
   test_controller.build_mpc();
   test_controller.solve();
+  u_init = test_controller.u;
+  for (int i = 0; i < 10; i++) {
+    test_controller.linearize();
+    test_controller.discretize();
+    test_controller.build_mpc();
+    test_controller.solve();
+  }
   std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
   std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
-  REQUIRE(std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() < 20000);
+  REQUIRE((std::abs(test_controller.u(0) - u_init(0)) < 1e-8 && std::abs(test_controller.u(1) - u_init(1)) < 1e-8 && std::abs(test_controller.u(2) - u_init(2)) < 1e-8 && std::abs(test_controller.u(3) - u_init(3)) < 1e-8));
 };
-
-
