ad609da4a6c4e12e0436ee5b7f0078b7086ed32b
Jad Wehbeh <jadwehbeh97@gmail.com>
2021-11-10 14:50:54 -0500

    Re-add test for runtime

--- test/test.cpp
+++ test/test.cpp
@@ -57,6 +57,32 @@ TEST_CASE("Inverted Hover")
   REQUIRE((std::abs(test_controller.u(0) + 2.4525) < 0.05 && std::abs(test_controller.u(1) + 2.4525) < 0.05 && std::abs(test_controller.u(2) + 2.4525) < 0.05 && std::abs(test_controller.u(3) + 2.4525) < 0.05));
 };
 
+TEST_CASE("Runtime")
+{
+  Eigen::VectorXd x_lin = Eigen::VectorXd(18);
+  Eigen::VectorXd u_lin = Eigen::VectorXd(4);
+  Eigen::VectorXd Y = Eigen::VectorXd::Zero(8 * 12);
+  x_lin << 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0.;
+  u_lin << 2.4525, 2.4525, 2.4525, 2.4525;
+  for (int i = 0; i < 8; i++) {
+    Y(i * 12 + 3) = 1.;
+    Y(i * 12 + 7) = 1.;
+    Y(i * 12 + 11) = 1.;
+  }
+  LieMPC test_controller;
+  test_controller.x = x_lin;
+  test_controller.u_lin = u_lin;
+  test_controller.Y = Y;
+  std::chrono::steady_clock::time_point tic = std::chrono::steady_clock::now();
+  test_controller.linearize();
+  test_controller.discretize();
+  test_controller.build_mpc();
+  test_controller.solve();
+  std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
+  std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
+  REQUIRE(std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() < 20000);
+};
+
 TEST_CASE("Consistency")
 {
   Eigen::VectorXd x_lin = Eigen::VectorXd(18);
